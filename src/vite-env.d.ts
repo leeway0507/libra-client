cf78b189d0f238c4d9178afae61b42c20e3b0587
leeway <niv1589@knou.ac.kr>
2024-12-23 16:01:56 +0900

    chore: update envset and add vitest

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite-plugin-pages/client-react" />
+
+
+interface ImportMetaEnv {
+    readonly VITE_BACKEND_API: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
