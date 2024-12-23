3931338e0d6e72b050df5843e3215d1dc6b9679e
leeway <niv1589@knou.ac.kr>
2024-12-23 22:47:33 +0900

    test cors policy

--- src/pages/backend/index.tsx
+++ src/pages/backend/index.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     const backendUrl = import.meta.env.VITE_BACKEND_API
+    console.log(backendUrl)
     const staticImageUrl = new URL("static/img/1234.jpg",backendUrl)
 
     return <img src={staticImageUrl.toString()} />
-    
 }
