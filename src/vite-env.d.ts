8ea1f866b6e1063bf85fb5997032b7a95be98173
leeway <niv1589@knou.ac.kr>
2024-12-25 07:57:05 +0900

    chore: add chakra-ui

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,9 @@
 /// <reference types="vite-plugin-pages/client-react" />
 
-
 interface ImportMetaEnv {
-    readonly VITE_BACKEND_API: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+	readonly VITE_BACKEND_API: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react-swc";
 import Pages from "vite-plugin-pages";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react({ jsxImportSource: "@emotion/react" }), Pages({ dirs: "src/pages", routeStyle: "next" })],
+	plugins: [
+		react({ jsxImportSource: "@emotion/react" }),
+		Pages({ dirs: "src/pages", routeStyle: "next" }),
+		tsconfigPaths(),
+		svgr(),
+	],
 });
