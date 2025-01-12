be001f3c023d89434b190439cffa615aad383707
leeway <niv1589@knou.ac.kr>
2025-01-13 00:21:22 +0900

    refactor:separate district object

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -17,6 +17,7 @@ import {
 } from "@/components/ui/dialog";
 
 import SelectSearch from "@/components/select-search";
+import { district } from "./district";
 
 export type LibInfo = {
 	libCode: string;
@@ -138,40 +139,6 @@ function LibItem({ item, onRemove }: { item: LibInfo; onRemove?: () => void }) {
 		</Flex>
 	);
 }
-
-type District = {
-	[key: string]: number;
-};
-
-const district: District = {
-	강남구: 1,
-	강동구: 2,
-	강북구: 3,
-	강서구: 4,
-	관악구: 5,
-	광진구: 6,
-	구로구: 7,
-	금천구: 8,
-	노원구: 9,
-	도봉구: 10,
-	동대문구: 11,
-	동작구: 12,
-	마포구: 13,
-	서대문구: 14,
-	서초구: 15,
-	성동구: 16,
-	성북구: 17,
-	송파구: 18,
-	양천구: 19,
-	영등포구: 20,
-	용산구: 21,
-	은평구: 22,
-	종로구: 23,
-	중구: 24,
-	중랑구: 25,
-	교육청: 26,
-	서울시: 27,
-};
 
 const OperatingTime = ({ children }: { children: React.ReactNode }) => {
 	return (
